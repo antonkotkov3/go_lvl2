de3c25b35f2d3e62453880acd3da3364cbfbb441
antonkotkov3 <93485645+antonkotkov3@users.noreply.github.com>
2021-11-04 21:47:06 +0300

    Add files via upload
    Напишите документацию для одной из предыдущих программ. Запустите сервер документации локально. Убедитесь, что документация отображается корректно.

---
+++ doc/doc.go
@@ -0,0 +1,12 @@
+
+// Функция AreaFunction рассчитывет и возврщает площадь фигуры
+//
+// AreaFunction(a,b,c int) int
+//
+// Функция AreaFunction(a,b,c) принимает три знчения типа int и возврщает одно знчение типa int
+package doc
+
+// MyFunction returns a string
+func AreaFunction(a,b,c int) int {
+	return a*b*c
+}
